36117436ff88d655188b47ab3f06fd841906d6ff
Hamed-Ajaj <hamedsgaming@gmail.com>
2023-10-11 18:04:54 +0300

    new Changes added

--- src/data/data.js
+++ src/data/data.js
@@ -1,35 +1,37 @@
 import WorkIm from '../assets/projects/workImg.jpeg'
 import RealEst from '../assets/projects/realestate.jpg'
 import expenseTracker from '../assets/projects/Screenshot_1.png'
-
+import HomeWeb from '../assets/projects/Screenshot_4.png'
+import movieApp from '../assets/projects/Screenshot_2.png'
+import weatherApp from '../assets/projects/Screenshot_3.png'
  export const data=[
     {
         id:1,
-        name:"React JS Application",
+        name:"Expense Tracker",
         image:expenseTracker,
-        github:"",
-        live:"",
+        github:"https://github.com/Hamed-Ajaj/Expense-Tracker.git",
+        live:"https://expense-tracker-amber-kappa.vercel.app/",
     },
     {
         id:2,
-        name:"React JS Application",
-        image:RealEst,
+        name:"Movie App",
+        image:movieApp,
         github:"",
         live:"",
     },
     {
         id:3,
-        name:"React JS Application",
-        image:WorkIm,
+        name:"Weather App",
+        image:weatherApp,
         github:"",
         live:"",
     },
     {
         id:4,
-        name:"React JS Application",
-        image:RealEst,
-        github:"",
-        live:"",
+        name:"Home Decoration Web",
+        image:HomeWeb,
+        github:"https://github.com/Hamed-Ajaj/home-decoration.git",
+        live:"https://tourmaline-meerkat-6f58ab.netlify.app/",
     },
 
 
